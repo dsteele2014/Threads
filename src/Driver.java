9d4991fdbaccf4350a25ad12a395f40480d11e2e
dsteele2014 <dsteele4444@gmail.com>
2018-02-14 12:04:25 -0500

    Add files via upload

--- src/Driver.java
+++ src/Driver.java
@@ -4,15 +4,24 @@ public class Driver{
             if(Integer.parseInt(args[0])<0)
                 System.err.println(args[0]+" must be >=0.");
             else {
-                UserInput total = new UserInput();
-                int upper = Integer.parseInt(args[0]);
-                Thread thrd = new Thread(new MatrixMultiplication());
-                thrd.start();
+                UserInput input = new UserInput();
+                int [][] matrix1= input.fillMatrix();
+                int [][] matrix2= input.fillMatrix();
+                int [][] matrix3= new int[matrix1[0].length][matrix2.length];
+                WorkerThread [][] Threads
+                final int NUM_THREADS = (matrix1[0].length*matrix2.length);
+                Thread[] workers = new Thread[NUM_THREADS];
+                for(int i =0; i<=NUM_THREADS; i++){
+                    workers[i] =  MatrixMultiplication(1,2,matrix1,matrix2,matrix3);
+                }
+                for(int i = 0; i<NUM_THREADS; i++)
                 try {
-                    thrd.join();
-                    System.out.println("The resulting of multiplying the matrices ""is " +);
+                    workers[i].join();
                 } catch (InterruptedException e) {}
             }
         }
+
     }
+
+
 }
